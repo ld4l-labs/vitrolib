6a2e0f22e228abbe71e98a85e4334d778669752d
hudajkhan <hjk54@cornell.edu>
2017-03-23 23:54:16 -0400

    updating autocomplete/field options

--- api/src/main/java/edu/cornell/mannlib/vitro/webapp/controller/ajax/CustomFormAJAXController.java
+++ api/src/main/java/edu/cornell/mannlib/vitro/webapp/controller/ajax/CustomFormAJAXController.java
@@ -6,6 +6,9 @@ import static edu.cornell.mannlib.vitro.webapp.modelaccess.ModelNames.USER_ACCOU
 import static javax.servlet.http.HttpServletResponse.SC_NOT_FOUND;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Constructor;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -22,7 +25,12 @@ import edu.cornell.mannlib.vitro.webapp.auth.permissions.SimplePermission;
 import edu.cornell.mannlib.vitro.webapp.auth.requestedAction.AuthorizationRequest;
 import edu.cornell.mannlib.vitro.webapp.controller.VitroRequest;
 import edu.cornell.mannlib.vitro.webapp.controller.ajax.SparqlUtils.AjaxControllerException;
+import edu.cornell.mannlib.vitro.webapp.edit.n3editing.VTwo.fields.ChildVClassesWithParent;
 import edu.cornell.mannlib.vitro.webapp.modelaccess.ModelAccess;
+import net.sf.json.JSON;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
 
 /**
  * Handle an AJAX request for a SPARQL query. On entry, the "query" parameter
@@ -34,7 +42,8 @@ public class CustomFormAJAXController extends VitroAjaxController {
 	private static final Log log = LogFactory
 			.getLog(CustomFormAJAXController.class);
 
-	public static final String PARAMETER_QUERY = "configComponent";
+	public static final String CONFIG_PARAMETER = "configComponent";
+	public static final String FIELD_NAME_PARAMETER = "fieldName";
 	public static final String RESPONSE_MIME_TYPE = "application/javascript";
 
 	
@@ -51,10 +60,19 @@ public class CustomFormAJAXController extends VitroAjaxController {
 			throws ServletException, IOException {
 		try {
 			
-			//Get the object for config component
-			String parameterValue = vreq.getParameter(PARAMETER_QUERY);
+			//Get the object for config component, this is a stringifed representation of the JSON object
+			String parameterValue = vreq.getParameter(CONFIG_PARAMETER);
+			String fieldNameValue = vreq.getParameter(FIELD_NAME_PARAMETER);
 			if(StringUtils.isNotEmpty(parameterValue)) {
 				//create a json object from parameter value
+				JSONObject json = (JSONObject) JSONSerializer.toJSON(parameterValue);				
+				//Get type
+				String outputJSON = generateDropDown(json, fieldNameValue, vreq);
+				String output = outputJSON.toString();				
+				PrintWriter writer = response.getWriter();
+	            response.setContentType("application/json");
+	            writer.write(output);
+	            writer.close();
 			}
 			
 			
@@ -62,6 +80,55 @@ public class CustomFormAJAXController extends VitroAjaxController {
 			log.error(e.getMessage());
 			//response.sendError(e.getStatusCode());
 		}
+	}
+
+	private String generateDropDown(JSONObject json, String fieldNameValue, VitroRequest vreq) {
+		JSONArray types = json.getJSONArray("@type");
+		JSON optionsAsJSON = null;
+		//A switch statement would be better for readability - how would we do that?
+		//NOT assuming multiple types, so just get rid of 
+		//We don't need this type if it exists, this is a generic type
+		types.remove("forms:FieldOptions");
+		String JavaClassNamePrefix = "edu.cornell.mannlib.vitro.webapp.edit.n3editing.VTwo.fields.";
+		if(types.size() > 0) {
+			String type = types.getString(0);
+			String JAVAClassName = getJAVAClassName(type);
+			//Get the JAVA Class name for this
+			try {
+			switch(JAVAClassName) {
+				case "ChildVClassesWithParent":
+					JSONObject classJSON = json.getJSONObject("http://vitro.mannlib.cornell.edu/ns/vitro/CustomFormConfiguration#classUri");
+					String classURI = classJSON.getString("@id");
+					//Could use class for name, but why bother?
+					ChildVClassesWithParent cvp = new ChildVClassesWithParent(classURI);
+					//Get the options
+					Map<String, String> options = cvp.getOptions(null, fieldNameValue, vreq.getWebappDaoFactory());
+					optionsAsJSON = JSONSerializer.toJSON(options);
+					
+				break;
+				case "":
+				break;
+				default:
+			
+			}
+				
+			} catch(Exception ex) {
+				log.error("An error occurred in getting the FieldOptions class", ex);
+			}
+			
+			
+			
+		}
+		return (optionsAsJSON == null)? null: optionsAsJSON.toString();
+	}
+
+	private String getJAVAClassName(String type) {
+		if(StringUtils.isNotEmpty(type)) {
+			if(type.startsWith("forms:")) {
+				return type.substring("forms:".length());
+			}
+		}
+		return null;
 	}
 
 
